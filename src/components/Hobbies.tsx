c69a868e0c32df99204818c5823cabb2f6fbba68
Cameron Noemdo <cameronnoemdo@outlook.com>
2025-01-19 23:54:10 +0200

    Add the hobbies file that will display the user's hobbies

---
+++ src/components/Hobbies.tsx
@@ -0,0 +1,27 @@
+const Hobbies = () => {
+    return (
+        <>
+            <h2>Hobbies</h2>
+
+            <table width="100%">
+                <tr>
+                    <td>Outdoor activities</td>
+                    <td>Programming activities</td>
+                </tr>
+                <tr>
+                    <td>Participating in marathons</td>
+                    <td>Hackathons</td>
+                </tr>
+                <tr>
+                    <td>Rugby</td>
+                    <td>Automation/Scripting</td>
+                </tr>
+                <tr>
+                    <td>Hiking</td>
+                </tr>
+            </table>
+        </>
+    );
+};
+
+export default Hobbies
