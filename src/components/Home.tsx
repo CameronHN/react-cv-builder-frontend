7999944f5e67cc8a4d6a49396d051c9e2cc38df3
Cameron Noemdo <cameronnoemdo@outlook.com>
2025-01-19 23:56:37 +0200

    Add the details for the landing page of the website

---
+++ src/components/Home.tsx
@@ -0,0 +1,87 @@
+import { skillsData } from "./Details";
+import Headshot from "./Headshot";
+import { projectsData } from "./Projects";
+
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+interface Introduction {
+    profession: string;
+    location: string;
+    specialisation: string;
+    passion: string;
+    yearsOfExperience: number;
+}
+
+interface About {
+    recentInstitute: string;
+    major: string;
+}
+
+const skillsArr: string[] = skillsData.flatMap(skill => skill.skills).sort();
+
+const personDetails = {
+    firstName: "Jane",
+    lastName: "Doe"
+}
+
+const introductionDetails = {
+    profession: "Web Developer",
+    location: "Cape Town, South Africa",
+    specialisation: "Front-End Development",
+    passion: "creating beautiful and responsive websites",
+    yearsOfExperience: 5,
+}
+
+const aboutDetails = {
+    recentInstitute: "the University of Cape Town",
+    major: "Computer Science"
+}
+
+function personalGreeting(person: Person) {
+    return "Hello, I'm " + person.firstName + "  " + person.lastName
+}
+
+function introduction(detail: Introduction) {
+    return "I'm a " + detail.profession + " based in " + detail.location + ". I specialize in " + detail.specialisation + " and have a passion for " + detail.passion + ". With " + detail.yearsOfExperience + " years of experience, I bring a unique blend of creativity and technical skills to every project."
+}
+
+function about(detail: About) {
+    return "I graduated from " + detail.recentInstitute + " with a degree in " + detail.major + ". Over the years, I've had the privilege to work with amazing clients and companies, which has allowed me to grow both professionally and personally."
+}
+
+const Home = () => {
+    return (
+        <>
+            <Headshot />
+
+            <h2>{personalGreeting(personDetails)}</h2>
+
+            <p>{introduction(introductionDetails)}</p>
+
+            <h2>About Me</h2>
+
+            <p>{about(aboutDetails)}</p>
+
+            <h2>Projects</h2>
+
+            {projectsData.map((project, index) => (
+                <div key={index}>
+                    {project.name}
+                </div>
+            ))}
+
+            <h2> Skills</h2>
+
+            {skillsArr.map((skill, index) => (
+                <div key={index}>
+                    {skill}
+                </div>
+            ))}
+        </>
+    );
+};
+
+export default Home
