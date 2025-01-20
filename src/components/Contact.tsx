1cdcba705f33e98c53372dd27536601921b26490
Cameron Noemdo <cameronnoemdo@outlook.com>
2025-01-20 15:17:49 +0200

    Add buttons to the social media so that it redirects to the appropriate website

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,5 +1,6 @@
 import { useForm } from "@mantine/form";
 import { TextInput, Textarea, Button, Box } from "@mantine/core";
+import { FaPhone, FaLinkedin, FaAt } from "react-icons/fa6";
 
 const Contact = () => {
 
@@ -27,14 +28,16 @@ const Contact = () => {
     <>
       <h2>Reach out to me!</h2>
 
-      <p><a href="#" target="_blank">Telephone</a> | <a href="https://linkedin.com" target="_blank">LinkedIn</a> | <a href="#">Email</a></p>
+      <a href="https://www.google.com" target="_blank"><Button variant="default" leftSection={<FaPhone size={14} />}>  Phone</Button></a>
+      <a href="https://www.linkedin.com" target="_blank"><Button variant="default" leftSection={<FaLinkedin size={14} />}>  LinkedIn</Button></a>
+      <a href="https://www.google.com" target="_blank"><Button variant="default" leftSection={<FaAt size={14} />}>  Email</Button></a>
 
       <h2>Or pop me a message!</h2>
 
       <Box maw={500} mx={"auto"}>
         <form id="contact-form" onSubmit={form.onSubmit(handleSubmit)}>
-          <TextInput label="Name:" placeholder="Your name" key={form.key('name')} {...form.getInputProps("name")} />
-          <TextInput label="Email:" placeholder="Your email" key={form.key('email')} mt="md" {...form.getInputProps("email")} />
+          <TextInput label="Name:" placeholder="Your name" key={form.key('name')} withAsterisk {...form.getInputProps("name")} />
+          <TextInput label="Email:" placeholder="Your email" key={form.key('email')} mt="md" withAsterisk {...form.getInputProps("email")} />
           <Textarea label="Message:" placeholder="Your message" key={form.key('message')} required mt="md" rows={7} cols={40} {...form.getInputProps("message")} />
           <Button type="submit" mt="md">Submit</Button>
         </form>
